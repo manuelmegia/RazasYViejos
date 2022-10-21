e46e0aa01961fb4bf6266d4b94ed9bc7e93581b5
ManueMegia <manmeg423@alu.centroafuera.es>
2022-10-21 14:15:53 +0200

    falta un poco

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,71 +1,92 @@
+import java.net.InterfaceAddress
 import java.util.*
 
 fun main(args: Array<String>) {
-    var lenin: Anciano = Elfo("Stalin", Raza.ENANO)
+    var lenin: Anciano = Enano("Stalin")
     lenin.pregunta("¿AAAAAAAAA?")
 }
 
-open class Persona(var nombre: String, var raza: Raza) : Anciano, Adulto, Joven {
-    override fun habla(respuesta: String) {
-        if (raza == Raza.ELFO) println(respuesta.rot13cifrado(13))
-        if (raza == Raza.GOBLIN) println(respuesta.rot13cifrado(13))
-        if (raza == Raza.ENANO) println(respuesta.uppercase(Locale.getDefault()))
-        if (raza == Raza.HUMANO) println(respuesta)
+open class Persona(var nombre: String) : Anciano, Adulto, Joven {
+    override fun idioma(respuesta: String) {
     }
 
     override fun pregunta(pregunta: String) {
         var respuesta = ""
-        if ()
-        respuesta = respuestasViejo(pregunta)
-        habla(respuesta)
+        if (test() == "Viejo") respuesta = respuestasViejo(pregunta)
+        else if (test() == "Adulto") respuesta = respuestasAdulto(pregunta)
+        else if (test() == "Anciano") respuesta = respuestasJoven(pregunta)
+        idioma(respuesta)
     }
 
     override fun respuestasViejo(pregunta: String): String {
         var cont = 0
         for (c in pregunta.toCharArray()) {
-            if (c.isUpperCase()) cont++
-            else if (c == '¿' || c== '?') cont + 2
+            if (c == '¿' || c == '?') cont += 2
+            else if (c.isUpperCase()) cont++
         }
 
         return if (pregunta == "¿Como estás?") "No me puedo mover"
         else if (cont == pregunta.length) "Háblame más alto que no te escucho"
         else if (cont == pregunta.length + 2) "Que no te escucho!"
-        else if (pregunta.equals(nombre)) "Las 5 de la tarde"
+        else if (pregunta == nombre) "Las 5 de la tarde"
         else "En mis tiempos esto no pasaba"
     }
 
     override fun respuestasAdulto(pregunta: String): String {
         var cont = 0
         for (c in pregunta.toCharArray()) {
-            if (c.isUpperCase()) cont++
-            else if (c == '¿' || c== '?') cont + 2
+            if (c.equals('¿') || c.equals('?')) cont += 2
+            else if (c.isUpperCase()) cont++
         }
 
-        return if (pregunta == "¿Como estás?") "No me puedo mover"
-        else if (cont == pregunta.length) "Háblame más alto que no te escucho"
-        else if (cont == pregunta.length + 2) "Que no te escucho!"
-        else if (pregunta.equals(nombre)) "Las 5 de la tarde"
-        else "En mis tiempos esto no pasaba"
+        return if (pregunta == "¿Como estás?") "En la flor de la vida, pero me empieza a doler la espalda"
+        else if (cont == pregunta.length) "No me levantes la voz mequetrefe"
+        else if (cont == pregunta.length + 2) "Estoy buscando la mejor solución"
+        else if (pregunta.equals(nombre)) "¿Necesitas algo?"
+        else "No sé de qué me estás hablando"
+    }
+
+    override fun test(): String {
+        return "adsdasdasd"
     }
 
     override fun respuestasJoven(pregunta: String): String {
         var cont = 0
         for (c in pregunta.toCharArray()) {
-            if (c.isUpperCase()) cont++
-            else if (c == '¿' || c== '?') cont + 2
+            if (c == '¿' || c == '?') cont += 2
+            else if (c.isUpperCase()) cont++
         }
 
-        return if (pregunta == "¿Como estás?") "No me puedo mover"
-        else if (cont == pregunta.length) "Háblame más alto que no te escucho"
-        else if (cont == pregunta.length + 2) "Que no te escucho!"
-        else if (pregunta.equals(nombre)) "Las 5 de la tarde"
-        else "En mis tiempos esto no pasaba"
+        return if (pregunta == "¿Como estás?") "De lujo"
+        else if (cont == pregunta.length) "Eh relájate"
+        else if (cont == pregunta.length + 2) "Tranqui se lo que hago"
+        else if (pregunta.equals(nombre)) "¿Qué pasa?"
+        else "Yo que se"
     }
-
 }
 
-class Elfo(nombre: String, raza: Raza) : Persona(nombre, raza) {
-    var martilloElfico = "Martillo Elfico"
+class Elfo(nombre: String) : Persona(nombre) {
+    override fun idioma(respuesta: String) {
+        println(respuesta.rot13cifrado(13))
+    }
+}
+
+class Goblin(nombre: String) : Persona(nombre) {
+    override fun idioma(respuesta: String) {
+        println(respuesta.rot13cifrado(13))
+    }
+}
+
+class Enano(nombre: String) : Persona(nombre) {
+    override fun idioma(respuesta: String) {
+        println(respuesta.uppercase(Locale.getDefault()))
+    }
+}
+
+class Humano(nombre: String) : Persona(nombre) {
+    override fun idioma(respuesta: String) {
+        println(respuesta)
+    }
 }
 
 fun String.rot13cifrado(tamañoRot: Int) = map {
@@ -98,18 +119,27 @@ enum class Raza {
 
 
 interface RespuestasRazas {
-    fun habla(respuesta: String)
+    fun idioma(respuesta: String)
     fun pregunta(pregunta: String)
 }
 
 interface Anciano : RespuestasRazas {
     fun respuestasViejo(pregunta: String): String
-}
+    fun test(): String {
+        return "Viejo"
+    }
+}//ayayayaya luego llamo al metodo ese y ya lo puedo reconocer. Si y aun así lo puedes sobreescribir
 
 interface Adulto : RespuestasRazas {
     fun respuestasAdulto(pregunta: String): String
+    fun test(): String {
+        return "Adulto"
+    }
 }
 
 interface Joven : RespuestasRazas {
     fun respuestasJoven(pregunta: String): String
+    fun test(): String {
+        return "Joven"
+    }
 }
